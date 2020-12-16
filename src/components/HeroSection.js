58b245b13daf46b3c9b938aaa700f6afe65ffc95
Fattijenishbek <fattijenishbek01@gmail.com>
2020-12-17 05:41:30 +0600

    add changes

---
+++ src/components/HeroSection.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Button } from './Button';
+import '../App.css';
+import './HeroSection.css';
+import Video from './videos/video-1.mp4';
+
+function HeroSection(){
+    return(
+        <div className='hero-container'>
+            <video autoPlay loop muted > 
+                <source src={Video} type="video/mp4" />
+            </video>
+            <h1>ADVENTURE AWAITS </h1>
+            <p>LET'S ENJOY!</p>
+            <div className="hero-btns">
+                <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>TO UNIVERSE</Button>
+                <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>WATCH VIDEO <i className='far fa-play-circle' /></Button>
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection
---
+++ src/components/Navbar.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from 'react';
+import {Link} from 'react-router-dom';
+import bts_logo from '../bts_logo.png';
+import './Navbar.css';
+import {Button} from './Button';
+
+function Navbar(){
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
+    const handleClick=()=>setClick(!click);
+    const closeMobileMenu=()=>setClick(false);
+
+    const showButton = ()=> {
+        if(window.innerWidth<=960){
+            setButton(false);
+        }else{
+            setButton(true);
+        }
+    };
+
+    useEffect(()=> {
+        showButton()
+    },[]);
+
+    // useEffect(()=> {
+    //     showButton()
+    // }, []);
+
+    window.addEventListener('resize', showButton);
+
+    return(
+        <div>
+            <nav className='navbar'>
+                <div className='navbar-container'>
+                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+                            BTS <i className="navbar-brand mr-auto" href="#"><img src={bts_logo} width="25" height="25" /></i>
+                    </Link>
+                    <div className='menu-icon' onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/pictures' className='nav-links' onClick={closeMobileMenu}>
+                                Pictures
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/army' className='nav-links' onClick={closeMobileMenu}>
+                                Army
+                            </Link>
+                        </li>
+                        {/* <li className='nav-item'>
+                            <Link to='/bighit' className='nav-links' onClick={closeMobileMenu}>
+                                BigHit
+                            </Link>
+                        </li> */}
+                        <li className='nav-item'>
+                            <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                                Sign Up
+                            </Link>
+                        </li>
+                    </ul>
+                    {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
